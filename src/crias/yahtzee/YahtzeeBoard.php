c62ae236ad675bfc549ce38a0ba1f1a09ec72b3c
Crias <travis.calder@gmail.com>
2014-01-24 21:01:18 +0000

    Add remaining upper-board fields (3-6) - No bonus yet.

--- src/crias/yahtzee/YahtzeeBoard.php
+++ src/crias/yahtzee/YahtzeeBoard.php
@@ -6,14 +6,34 @@ use crias\None;
 class YahtzeeBoard {
   private $ones;
   private $twos;
-  
+  private $threes;
+  private $fours;
+  private $fives;
+  private $sixes;
+
   public function __construct() {
     $this->ones = new None;
     $this->twos = new None;
+    $this->threes = new None;
+    $this->fours = new None;
+    $this->fives = new None;
+    $this->sixes = new None;
   }
   
+  // Intentionally bad implementation - fix it with a test!
   public function totalScore() {
-    return $this->ones->getOrElse(0) + $this->twos->getOrElse(0);
+    return 
+      $this->ones->getOrElse(
+        $this->twos->getOrElse(
+          $this->threes->getOrElse(
+            $this->fours->getOrElse(
+              $this->fives->getOrElse(
+                $this->sixes->getOrElse(0)
+              )
+            )
+          )
+        )
+      );
   }
   
   public function scoreOnes(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
@@ -22,6 +42,22 @@ class YahtzeeBoard {
   
   public function scoreTwos(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
     $this->twos = $this->twos->orElse($this->sumValuesEqualTo(2, [$first, $second, $third, $fourth, $fifth]));
+  }
+
+  public function scoreThrees(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $this->threes = $this->threes->orElse($this->sumValuesEqualTo(3, [$first, $second, $third, $fourth, $fifth]));
+  }
+
+  public function scoreFours(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $this->fours = $this->fours->orElse($this->sumValuesEqualTo(4, [$first, $second, $third, $fourth, $fifth]));
+  }
+
+  public function scoreFives(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $this->fives = $this->fives->orElse($this->sumValuesEqualTo(5, [$first, $second, $third, $fourth, $fifth]));
+  }
+
+  public function scoreSixes(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $this->sixes = $this->sixes->orElse($this->sumValuesEqualTo(6, [$first, $second, $third, $fourth, $fifth]));
   }
 
   private function sumValuesEqualTo($value, array $dice) {
--- tests/crias/yahtzee/YahtzeeBoardTest.php
+++ tests/crias/yahtzee/YahtzeeBoardTest.php
@@ -9,6 +9,8 @@ class YahtzeeBoardTest extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals(0, $y->totalScore());
   }
+  
+  /* Ones */
 
   public function testOnes_totalScoreShouldReturnScoreEqualToTotalNumberOfOnes_whenScoringOnes() {
     $this->assertEquals(0, $this->boardWithOnes(dice(2), dice(3), dice(4), dice(5), dice(6))->totalScore());
@@ -40,6 +42,8 @@ class YahtzeeBoardTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(3, $y->totalScore());
   }
 
+  /* Twos */
+
   public function testTwos_totalScoreShouldReturnScoreEqualToTotalNumberOfTwos_whenScoringTwos() {
     $this->assertEquals(0, $this->boardWithTwos(dice(1), dice(3), dice(4), dice(5), dice(6))->totalScore());
     $this->assertEquals(2, $this->boardWithTwos(dice(2), dice(3), dice(4), dice(5), dice(6))->totalScore());
@@ -70,6 +74,136 @@ class YahtzeeBoardTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(6, $y->totalScore());
   }
 
+  /* Threes */
+
+  public function testThrees_totalScoreShouldReturnScoreEqualToTotalNumberOfThrees_whenScoringThrees() {
+    $this->assertEquals(0, $this->boardWithThrees(dice(1), dice(2), dice(4), dice(5), dice(6))->totalScore());
+    $this->assertEquals(3, $this->boardWithThrees(dice(3), dice(2), dice(4), dice(5), dice(6))->totalScore());
+    $this->assertEquals(6, $this->boardWithThrees(dice(3), dice(3), dice(4), dice(5), dice(6))->totalScore());
+    $this->assertEquals(9, $this->boardWithThrees(dice(3), dice(3), dice(3), dice(5), dice(6))->totalScore());
+    $this->assertEquals(12, $this->boardWithThrees(dice(3), dice(3), dice(3), dice(3), dice(6))->totalScore());
+    $this->assertEquals(15, $this->boardWithThrees(dice(3), dice(3), dice(3), dice(3), dice(3))->totalScore());
+  }
+  
+  public function testThrees_positionOfTheDiceDoesntMatter() {
+    $this->assertEquals(3, $this->boardWithThrees(dice(3), dice(1), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(3, $this->boardWithThrees(dice(1), dice(3), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(3, $this->boardWithThrees(dice(1), dice(1), dice(3), dice(1), dice(1))->totalScore());
+    $this->assertEquals(3, $this->boardWithThrees(dice(1), dice(1), dice(1), dice(3), dice(1))->totalScore());
+    $this->assertEquals(3, $this->boardWithThrees(dice(1), dice(1), dice(1), dice(1), dice(3))->totalScore());
+  }
+
+  public function testThrees_cannotModifyThreesAfterTheyAreScored() {
+    $y = new YahtzeeBoard();
+    
+    $y->scoreThrees(dice(3), dice(1), dice(3), dice(2), dice(3));
+    $this->assertEquals(9, $y->totalScore());
+
+    $y->scoreThrees(dice(3), dice(3), dice(3), dice(3), dice(3)); // Should this throw?
+    $this->assertEquals(9, $y->totalScore());
+
+    $y->scoreThrees(dice(1), dice(2), dice(4), dice(5), dice(6)); // Should this throw?
+    $this->assertEquals(9, $y->totalScore());
+  }
+  
+  /* Fours */
+
+  public function testFours_totalScoreShouldReturnScoreEqualToTotalNumberOfFours_whenScoringFours() {
+    $this->assertEquals(0, $this->boardWithFours(dice(1), dice(2), dice(3), dice(5), dice(6))->totalScore());
+    $this->assertEquals(4, $this->boardWithFours(dice(4), dice(2), dice(3), dice(5), dice(6))->totalScore());
+    $this->assertEquals(8, $this->boardWithFours(dice(4), dice(4), dice(3), dice(5), dice(6))->totalScore());
+    $this->assertEquals(12, $this->boardWithFours(dice(4), dice(4), dice(4), dice(5), dice(6))->totalScore());
+    $this->assertEquals(16, $this->boardWithFours(dice(4), dice(4), dice(4), dice(4), dice(6))->totalScore());
+    $this->assertEquals(20, $this->boardWithFours(dice(4), dice(4), dice(4), dice(4), dice(4))->totalScore());
+  }
+  
+  public function testFours_positionOfTheDiceDoesntMatter() {
+    $this->assertEquals(4, $this->boardWithFours(dice(4), dice(1), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(4, $this->boardWithFours(dice(1), dice(4), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(4, $this->boardWithFours(dice(1), dice(1), dice(4), dice(1), dice(1))->totalScore());
+    $this->assertEquals(4, $this->boardWithFours(dice(1), dice(1), dice(1), dice(4), dice(1))->totalScore());
+    $this->assertEquals(4, $this->boardWithFours(dice(1), dice(1), dice(1), dice(1), dice(4))->totalScore());
+  }
+
+  public function testFours_cannotModifyFoursAfterTheyAreScored() {
+    $y = new YahtzeeBoard();
+    
+    $y->scoreFours(dice(4), dice(1), dice(4), dice(2), dice(4));
+    $this->assertEquals(12, $y->totalScore());
+
+    $y->scoreFours(dice(4), dice(4), dice(4), dice(4), dice(4)); // Should this throw?
+    $this->assertEquals(12, $y->totalScore());
+
+    $y->scoreFours(dice(1), dice(2), dice(3), dice(5), dice(6)); // Should this throw?
+    $this->assertEquals(12, $y->totalScore());
+  }  
+
+  /* Fives */
+
+  public function testFives_totalScoreShouldReturnScoreEqualToTotalNumberOfFives_whenScoringFives() {
+    $this->assertEquals(0, $this->boardWithFives(dice(1), dice(2), dice(3), dice(4), dice(6))->totalScore());
+    $this->assertEquals(5, $this->boardWithFives(dice(5), dice(2), dice(3), dice(4), dice(6))->totalScore());
+    $this->assertEquals(10, $this->boardWithFives(dice(5), dice(5), dice(3), dice(4), dice(6))->totalScore());
+    $this->assertEquals(15, $this->boardWithFives(dice(5), dice(5), dice(5), dice(4), dice(6))->totalScore());
+    $this->assertEquals(20, $this->boardWithFives(dice(5), dice(5), dice(5), dice(5), dice(6))->totalScore());
+    $this->assertEquals(25, $this->boardWithFives(dice(5), dice(5), dice(5), dice(5), dice(5))->totalScore());
+  }
+  
+  public function testFives_positionOfTheDiceDoesntMatter() {
+    $this->assertEquals(5, $this->boardWithFives(dice(5), dice(1), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(5, $this->boardWithFives(dice(1), dice(5), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(5, $this->boardWithFives(dice(1), dice(1), dice(5), dice(1), dice(1))->totalScore());
+    $this->assertEquals(5, $this->boardWithFives(dice(1), dice(1), dice(1), dice(5), dice(1))->totalScore());
+    $this->assertEquals(5, $this->boardWithFives(dice(1), dice(1), dice(1), dice(1), dice(5))->totalScore());
+  }
+
+  public function testFives_cannotModifyFivesAfterTheyAreScored() {
+    $y = new YahtzeeBoard();
+    
+    $y->scoreFives(dice(5), dice(1), dice(5), dice(2), dice(5));
+    $this->assertEquals(15, $y->totalScore());
+
+    $y->scoreFives(dice(5), dice(5), dice(5), dice(5), dice(5)); // Should this throw?
+    $this->assertEquals(15, $y->totalScore());
+
+    $y->scoreFives(dice(1), dice(2), dice(3), dice(4), dice(6)); // Should this throw?
+    $this->assertEquals(15, $y->totalScore());
+  }  
+
+  /* Sixes */
+
+  public function testSixes_totalScoreShouldReturnScoreEqualToTotalNumberOfSixes_whenScoringSixes() {
+    $this->assertEquals(0, $this->boardWithSixes(dice(1), dice(2), dice(3), dice(4), dice(5))->totalScore());
+    $this->assertEquals(6, $this->boardWithSixes(dice(6), dice(2), dice(3), dice(4), dice(5))->totalScore());
+    $this->assertEquals(12, $this->boardWithSixes(dice(6), dice(6), dice(3), dice(4), dice(5))->totalScore());
+    $this->assertEquals(18, $this->boardWithSixes(dice(6), dice(6), dice(6), dice(4), dice(5))->totalScore());
+    $this->assertEquals(24, $this->boardWithSixes(dice(6), dice(6), dice(6), dice(6), dice(5))->totalScore());
+    $this->assertEquals(30, $this->boardWithSixes(dice(6), dice(6), dice(6), dice(6), dice(6))->totalScore());
+  }
+  
+  public function testSixes_positionOfTheDiceDoesntMatter() {
+    $this->assertEquals(6, $this->boardWithSixes(dice(6), dice(1), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(6, $this->boardWithSixes(dice(1), dice(6), dice(1), dice(1), dice(1))->totalScore());
+    $this->assertEquals(6, $this->boardWithSixes(dice(1), dice(1), dice(6), dice(1), dice(1))->totalScore());
+    $this->assertEquals(6, $this->boardWithSixes(dice(1), dice(1), dice(1), dice(6), dice(1))->totalScore());
+    $this->assertEquals(6, $this->boardWithSixes(dice(1), dice(1), dice(1), dice(1), dice(6))->totalScore());
+  }
+
+  public function testSixes_cannotModifySixesAfterTheyAreScored() {
+    $y = new YahtzeeBoard();
+    
+    $y->scoreSixes(dice(6), dice(1), dice(6), dice(2), dice(6));
+    $this->assertEquals(18, $y->totalScore());
+
+    $y->scoreSixes(dice(6), dice(6), dice(6), dice(6), dice(6)); // Should this throw?
+    $this->assertEquals(18, $y->totalScore());
+
+    $y->scoreSixes(dice(1), dice(2), dice(3), dice(4), dice(5)); // Should this throw?
+    $this->assertEquals(18, $y->totalScore());
+  }  
+
+  /* Helpers */
+  
   private function boardWithOnes(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
     $y = new YahtzeeBoard();
     $y->scoreOnes($first, $second, $third, $fourth, $fifth);
@@ -82,4 +216,27 @@ class YahtzeeBoardTest extends PHPUnit_Framework_TestCase {
     return $y;
   }
 
+  private function boardWithThrees(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $y = new YahtzeeBoard();
+    $y->scoreThrees($first, $second, $third, $fourth, $fifth);
+    return $y;
+  }
+
+  private function boardWithFours(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $y = new YahtzeeBoard();
+    $y->scoreFours($first, $second, $third, $fourth, $fifth);
+    return $y;
+  }
+
+  private function boardWithFives(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $y = new YahtzeeBoard();
+    $y->scoreFives($first, $second, $third, $fourth, $fifth);
+    return $y;
+  }
+  
+  private function boardWithSixes(Dice $first, Dice $second, Dice $third, Dice $fourth, Dice $fifth) {
+    $y = new YahtzeeBoard();
+    $y->scoreSixes($first, $second, $third, $fourth, $fifth);
+    return $y;
+  }
 }
